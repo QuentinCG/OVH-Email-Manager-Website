cbaee5b12bde29c9b30a9f56a99ea49eff229d49
Quentin Comte-Gaz <quentin@comte-gaz.com>
2016-07-18 20:34:42 +0200

    website: Add header and footer to the website
    Add the header and the footer of the website.
    Those files will query the OVH API to get the
    email server status and the creation date of
    the email server.

---
+++ footer.php
@@ -0,0 +1,27 @@
+﻿<?php
+/*
+ * \brief Footer page integrated in all pages
+ *
+ * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
+ * \date 18 July 2016
+ * \license MIT License (contact me if too restrictive)
+ * \copyright Copyright (c) 2016 Quentin Comte-Gaz
+ * \version 1.0
+ */
+?>
+
+<!----------------------BEGIN FOOTER-------------------->
+  </div><!--Close site_content div instanciated in header-->
+  <footer>
+    <a href="index.php">Home</a> | <a href="informations.php">Information</a> | <a href="contact.php">Contact</a><br/><br/>
+    Website designed by <a target="_blank" href="http://quentin.comte-gaz.com/en">Quentin Comte-Gaz</a> | Using <a target="_blank" href="https://api.ovh.com/">OVH API</a>
+  </footer>
+
+  </div><!--Close main div instanciated in header-->
+
+  <!-- Javascript at the bottom for fast page loading -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+
+</body><!--Close body instanciated in header-->
+</html><!--Close html instanciated in header-->
+<!----------------------END FOOTER---------------------->
---
+++ header.php
@@ -0,0 +1,124 @@
+﻿<?php
+/*
+ * \brief header page integrated in all pages
+ *
+ * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
+ * \date 18 July 2016
+ * \license MIT License (contact me if too restrictive)
+ * \copyright Copyright (c) 2016 Quentin Comte-Gaz
+ * \version 1.0
+ */
+?>
+<!----------------------BEGIN HEADER------------------->
+<?php
+
+require_once(dirname(__FILE__).'/config.php');
+require_once(dirname(__FILE__).'/ovh_utility.php');
+
+$is_api_valid = true;
+try {
+  $domain_info = getEmailDomainInfo($api, $domain);
+} catch (Exception $e) {
+  $is_api_valid = false;
+  //echo "Impossible to get the email server info: ".$e->getMessage();
+}
+
+$title = "Email management system for ".$domain." domain";
+$slogan = "Manage your <span class='highlight'>".$domain."</span> email account";
+switch ($page_name) {
+  case "index.php":
+    break;
+  case "informations.php":
+    $title = "How to use your email account";
+    break;
+  case "contact.php":
+    $title = "Contact";
+    break;
+  default:
+    break;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title><?php echo $title; ?></title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="robots" content="noindex, nofollow">
+
+  <link rel="icon" type="image/png" href="favicon.ico" />
+
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" href="css/buttons.css" />
+  <link rel="stylesheet" type="text/css" href="css/progress_bar.css" />
+
+  <script type="text/javascript" src="js/progress_bar.js"></script>
+  <script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
+</head>
+
+<body>
+  <div id="main">
+    <header>
+      <div id="strapline">
+        <div id="welcome_slogan">
+          <h3><?php echo $slogan; ?></h3>
+        </div>
+      </div>
+      <nav>
+        <div id="menubar">
+          <ul id="nav">
+            <li <?php if($page_name == "index.php") { echo "class=\"current\""; } ?>><a href="index.php">Home</a></li>
+            <li <?php if($page_name == "informations.php") { echo "class=\"current\""; } ?>><a href="informations.php">Information</a></li>
+            <li <?php if($page_name == "contact.php") { echo "class=\"current\""; } ?>><a href="contact.php">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <div id="site_content">
+      <div class="sidebar_container">
+        <div class="sidebar">
+          <div class="sidebar_item">
+            <h2>Email server status</h2>
+            <p>
+            <?php
+              if($domain_info['status'] == "ok") {
+                echo '<a class="medium green button" target="_blank" href="'.$email_server.'">Active email server</a>';
+              } elseif ($is_api_valid == true) {
+                echo '<a class="medium orange button" href="contact.php">Email server is down</a>';
+              } else {
+                echo '<a class="medium orange button" href="contact.php">Impossible to check server status</a>';
+              }
+            ?>
+            <br/>
+            Creation date:
+            <?php
+              if (!$is_api_valid) {
+                echo "Impossible to get information";
+              } else {
+                $date = new DateTime($domain_info['creationDate']);
+                echo $date->format('j-M-Y');
+              }
+            ?>
+            <br/>
+            </p>
+          </div>
+        </div>
+      <!-- You may add additional side bars here: -->
+      <!--
+      <div class="sidebar">
+        <div class="sidebar_item">
+      <h2>Latest update</h2>
+      <h3></h3>
+      <p></p>
+        </div>
+      </div>
+      -->
+    </div>
+    <!--site_content not closed-->
+  <!--div id="main" not closed-->
+<!--body not closed-->
+<!--html not closed-->
+<!----------------------END HEADER--------------------->
